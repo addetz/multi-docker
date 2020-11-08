1bab11f8767478ed28dd6b3240af91e271b5dee2
addetz <adelinasimion@gmail.com>
2020-11-08 21:39:49 +0000

    removed bad char

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Adelina is the best! Multi docker bbz! <3
+            Adelina is the best! Multi docker bbz!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
